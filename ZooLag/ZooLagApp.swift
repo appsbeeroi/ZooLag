6c24296664900a5de1f469b327c42403af4fcb1e
esplusnin <esplusnin94@yandex.ru>
2025-11-06 08:17:22 +0300

    Initial Commit

---
+++ ZooLag/ZooLagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZooLagApp.swift
+//  ZooLag
+//
+//  Created by Евгений on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZooLagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
